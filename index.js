e18a737c063ff967056af6b48287e27baedccaf7
David Guan <DavidGuanDev@gmail.com>
2016-06-22 21:50:55 +0800

    Set up basic express application.

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 import express from 'express'
+import path from 'path'
+import logger from 'morgan'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import { Strategy } from 'passport-local'
 
-express()
-  .listen(3000)
+import routes from './routes/index'
+import users from './routes/users'
+import Account from './models/account'
 
-console.log('GraphQL server running on http://localhost:3000/graphql')
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+//middleware settings
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(require('express-session')({
+    secret: 'reddit like website',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes)
+app.use('/users', users)
+
+passport.use(new Strategy(Account.authenticate()))
+passport.serializeUser(Account.serializeUser())
+passport.deserializeUser(Account.deserializeUser())
+
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4')
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(3000)
+
+console.log('Server running on http://localhost:3000')
---
+++ models/account.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import passportLocalMongoose from 'passport-local-mongoose'
+
+const { Schema } = mongoose
+
+const Account = new Schema({
+    username: String,
+    password: String
+})
+
+Account.plugin(passportLocalMongoose)
+
+export default mongoose.model('Account', Account)
