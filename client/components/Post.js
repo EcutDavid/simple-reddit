0c37816e77dbd60f4682c35a770348cc67ec7061
David Guan <davidguandev@gmail.com>
2016-06-27 21:47:18 +0800

    Hide vote buttons for non-user.

--- client/components/Post.js
+++ client/components/Post.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { connect } from 'react-redux'
 
 import 'styles/post.scss'
 
 export default class Post extends Component {
   render() {
-    const { name, description, points, id, author } = this.props
+    const { alreadyLogin, name, description, points, id, author } = this.props
     return (
       <div className='Post'>
         <Link to={`article/${id ? id : '0'}`} className='title'>{ name }</Link>
         <p className='description'>{ description }</p>
         <p className='points'>{ points }</p>
-        <button className='vote-button fa fa-arrow-up'/>
-        <button className='vote-button fa fa-arrow-down'/>
+        { alreadyLogin && <button className='vote-button fa fa-arrow-up'/> }
+        { alreadyLogin && <button className='vote-button fa fa-arrow-down'/> }
         { author ? <p>{ `Author: ${author}` }</p>: ''}
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    alreadyLogin: state.user.get('alreadyLogin')
+  }
+}
+
+export default connect(mapStateToProps)(Post)
